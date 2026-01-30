da1ff7ec8db8259bae910221a7ed560061d0763e
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-01-30 18:14:12 +0530

    made changes

--- main.py
+++ main.py
@@ -38,3 +38,19 @@ async def read_custom_file(file: str):
 @app.get('/fast-items/')
 async def read_item(skip: int = 0, limit: int = 10):#skip means how many items to skip before starting to collect the result set. skip=0 means start from the beginning.
     return fast_items_db[skip : skip + limit]
+#in the above example, limit specifies the maximum number of items to return. it's a query parameter with a default value of 10.
+#So, if you call /fast-items/?skip=1&limit=2, it will skip the first item and return the next two items from the fast_items_db list.
+#If you call /fast-items/ without any query parameters, it will return the first 10 items from the list.
+
+@app.get('/items/{item_id}')
+async def read_it(item_id:str, q: str| None = None, short: bool = False): #'q' should be treated as an optional query parameter. 
+                                                                          # If the client includes it in the request, 
+                                                                          # its value will be passed to the function; otherwise, it will be None.
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
