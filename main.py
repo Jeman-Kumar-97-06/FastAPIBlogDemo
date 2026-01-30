b5763e4b51e22e4b178cc9d0ff9d3e5982abab5e
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-01-30 16:56:29 +0530

    made changes

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from enum import Enum
 from fastapi import FastAPI
 app = FastAPI()
+
+#This means, we can define a set of string constants to represent different model names. 
+#The path parameter should be one of these predefined model names.
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 @app.get('/')
 async def root():
@@ -8,3 +16,14 @@ async def root():
 @app.get('/items/{item_id}')
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    elif model_name == ModelName.resnet:
+        return {"model_name": model_name, "message": "Residuals are cool!"}
+    elif model_name == ModelName.lenet:
+        return {"model_name": model_name, "message": "LeNet is the best!"}
+    return {"model_name": model_name, "message": "Unknown model"}
+
